5e3b2b6e82be7b7ba28c423c30a82f85124ba36b
Andrey Sokolov <sokolov-as-i@yandex.ru>
2018-08-27 16:32:20 +0300

    base server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
-import express from "express";
-import cors from "cors";
-import path from "path";
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-const staticPath = path.join(__dirname, "../public");
+const staticPath = path.join(__dirname, "../build");
 app.use(express.static(staticPath));
+
+app.get("/api", (req, res) => res.send("Hello, world"));
 
 app.listen(port, () => {
   console.log(`Server started at port: ${port}`);
